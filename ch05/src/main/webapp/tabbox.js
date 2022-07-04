02ee85ebbd623d167854d2de966dc4b72322026e
seoin1102 <im_in_@naver.com>
2022-07-04 17:52:33 +0900

    commit

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -1,23 +1,26 @@
 // DOM Event Model Level0: basic: HTML Element의 Event Attribute에 Event Handler(함수)
 // JavaScript Code(DOM API) 로만 작성하기
+
 var tabBox = {
-   _liSelected: null,
    init: function() {
-      window.onload = this._onWindowLoad.bind(this);
+      window.addEventListener('load', this._onWindowLoad.bind(this));
    },
-   _onTabClicked: function(){
-	   if (tabBox._liSelected != null) {
-	      tabBox._liSelected.className = '';
-   		}
-	   this.className = 'selected';
-	   tabBox._liSelected = this;
-	},
-   _onWindowLoad: function() {
-      document.getElementById("tab01").onclick = this.onTabClicked;
-      document.getElementById("tab02").onclick = this.onTabClicked;
-      document.getElementById("tab03").onclick = this.onTabClicked;
-      document.getElementById("tab04").onclick = this.onTabClicked;
-      document.getElementById("tab05").onclick = this.onTabClicked;
    
+   _onTabClicked: function() {
+      // unSelect
+      var lisSelected = document.getElementsByClassName('selected');
+      lisSelected.length == 1 && (lisSelected[0].className = '');
+
+      // Select
+      this.className = 'selected';
+   },
+   _onWindowLoad: function() {
+      var divTabBox = document.getElementsByClassName("tab-box")[0];
+      var ul = divTabBox.childNodes[1];
+      var liTabs = ul.getElementsByTagName("li");
+      
+      for(var i=0; i<liTabs.length; i++) {
+         liTabs[i].addEventListener("click", this._onTabClicked);
+      }
    }
 };
