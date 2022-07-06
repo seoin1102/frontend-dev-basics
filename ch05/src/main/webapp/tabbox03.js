26f95a70236146af730b613562d6ef6ce337d308
seoin1102 <im_in_@naver.com>
2022-07-06 10:20:07 +0900

    commit

--- ch05/src/main/webapp/tabbox03.js
+++ ch05/src/main/webapp/tabbox03.js
@@ -1,24 +1,14 @@
 // jQuery로 작성하기
-var tabBox = {
-   init: function() {
-      window.addEventListener('load', this._onWindowLoad.bind(this));
-   },
-   
-   _onTabClicked: function() {
-      // unSelect
-      var lisSelected = document.getElementsByClassName('selected');
-      lisSelected.length == 1 && (lisSelected[0].className = '');
 
-      // Select
-      this.className = 'selected';
-   },
-   _onWindowLoad: function() {
-      var divTabBox = document.getElementsByClassName("tab-box")[0];
-      var ul = divTabBox.childNodes[1];
-      var liTabs = ul.getElementsByTagName("li");
-      
-      for(var i=0; i<liTabs.length; i++) {
-         liTabs[i].addEventListener("click", this._onTabClicked);
-      }
-   }
-};
+
+$(function(){
+	var $obj = $('li');
+	$obj.click(function(){
+		for(let i = 0; i < $obj.length; i++){
+			if($($obj[i]).hasClass("selected")){
+				$($obj[i]).removeClass("selected");
+			}
+			}
+		$(this).hasClass("selected") ? $(this).removeClass("selected") : $(this).addClass("selected");
+	}) 
+})
